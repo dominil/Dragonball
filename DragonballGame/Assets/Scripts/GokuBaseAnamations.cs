281288f3bfe99fe40bbfc5aea4fb38eb00e86296
dominil <danicki.dominik@gmail.com>
2022-11-07 12:37:53 +0100

    animations

--- DragonballGame/Assets/Scripts/GokuBaseAnamations.cs
+++ DragonballGame/Assets/Scripts/GokuBaseAnamations.cs
@@ -4,24 +4,16 @@ using UnityEngine;
 
 public class GokuBaseAnamations : MonoBehaviour
 {
-    const string Goku_Base_Moving = "Moving";
-    const string Goku_Base_FastMoving = "FastMoving";
-    const string Goku_Base_Atak1 = "Atak1";
-    const string Goku_Base_Atak2 = "Atak2";
-    const string Goku_Base_Atak3 = "Atak3";
-    const string Goku_Base_combo1 = "combo1";
-    const string Goku_Base_combo2 = "combo2";
-    const string Goku_Base_GenkiDama = "GenkiDama";
-    const string Goku_Base_Kamehameha = "Kamehameha";
-    const string Goku_Base_kiBlast = "kiBlast";
-    const string Goku_Base_Dead = "Dead";
-    const string Goku_Base_Idle = "Idle";
-    private string currentState;
+
 
     PlayerMovement playermov;
     Animator animator;
     Rigidbody2D rb;
-    private int atak=1;
+
+    private int atak = 1;
+    private bool doindnothing = true;
+    private float delay;
+
     void Start()
     {
         playermov = GetComponent<PlayerMovement>();
@@ -29,55 +21,28 @@ public class GokuBaseAnamations : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-
-        
-        if (playermov.movement.x != 0||playermov.movement.y !=0)
+        if (playermov.movement != new Vector2(0, 0))
         {
-            ChangeAnimationState(Goku_Base_Moving);
+            SetRun();
         }
-        //if (playermov.movement.x == 0 || playermov.movement.y == 0)
-        //{
-        //    ChangeAnimationState(Goku_Base_Idle);
-        //}
-        
-
-        if (Input.GetKeyDown(KeyCode.H))
+        else
         {
-            rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
-            if (atak == 1) {
-                ChangeAnimationState(Goku_Base_Atak1);
-                atak++;
-              
-            }else if (atak == 2)
-            {
-                ChangeAnimationState(Goku_Base_Atak2);
-                atak++;
-            }else if (atak == 3)
-            {
-                ChangeAnimationState(Goku_Base_Atak3);
-                atak = 1;
-            }
-         
+            SetRunNo();
         }
-        
     }
-
-
-    public void ChangeAnimationState(string newState)
+    public void SetIdle()
     {
-        if (currentState == newState) return;
-
-        animator.Play(newState);
-
-        currentState = newState;
-    } 
-
-    public void LetFree()
+        animator.SetBool("idle", true);
+    }
+    public void SetRun()
     {
-        rb.constraints = RigidbodyConstraints2D.None;
-            }
-
+        animator.SetBool("ruszasie", true);
+    }
+    public void SetRunNo()
+    {
+        animator.SetBool("ruszasie", false);
+    }
+   
 }
